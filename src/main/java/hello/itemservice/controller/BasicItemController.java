235935a7a85f21f9d454b6249465732b0cd0342e
EUIJOON <euijoonism@gmail.com>
2023-09-23 13:31:06 +0900

    상품 등록

--- src/main/java/hello/itemservice/controller/BasicItemController.java
+++ src/main/java/hello/itemservice/controller/BasicItemController.java
@@ -40,6 +40,11 @@ public class BasicItemController {
     }
 
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
