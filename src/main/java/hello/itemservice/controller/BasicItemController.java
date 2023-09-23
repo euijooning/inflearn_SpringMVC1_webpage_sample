fe00ac6be14379c91787bfa30cea4ad792fe8558
EUIJOON <euijoonism@gmail.com>
2023-09-23 12:39:31 +0900

    상품 상세

--- src/main/java/hello/itemservice/controller/BasicItemController.java
+++ src/main/java/hello/itemservice/controller/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -27,6 +28,17 @@ public class BasicItemController {
         // "basic/items" 뷰 템플릿을 호출하여 상품 목록을 화면에 표시
         return "basic/items";
     }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        // 경로 변수로부터 받은 itemId를 사용하여 상품을 조회합니다.
+        Item item = itemRepository.findById(itemId);
+        // 조회한 상품을 모델에 추가합니다. 이후 뷰 템플릿에서 사용할 수 있습니다.
+        model.addAttribute("item", item);
+        // "basic/item" 뷰 템플릿을 호출하여 상품 상세 정보를 화면에 표시합니다.
+        return "basic/item";
+    }
+
 
     /**
      * 테스트용 데이터 추가
